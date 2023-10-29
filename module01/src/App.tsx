94a45709b2041ee5bc5edd3377d106374c2de761
Alexey0795 <alexey0795@mail.ru>
2023-10-29 21:16:35 +0300

    fix: App component

--- module01/src/App.tsx
+++ module01/src/App.tsx
@@ -1,41 +1,14 @@
-import { useState } from 'react';
+import { Component, ReactNode } from 'react';
 import SomeComponent from './SomeComponent.tsx';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
 import ErrorBoundary from './ErrorBoundary.tsx';
+import './App.css';
 
-function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <>
+export class App extends Component {
+  render(): ReactNode {
+    return (
       <ErrorBoundary>
         <SomeComponent />
       </ErrorBoundary>
-
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  );
+    );
+  }
 }
-
-export default App;
